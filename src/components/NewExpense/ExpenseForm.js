ca29e998a4b0953bd688bfea7b0ec38e87f8bd56
ThomasC119 <90348324+ThomasC119@users.noreply.github.com>
2022-05-12 17:09:59 -0400

    Input form work
    fleshing out input forms and two way binding

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,24 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  // ALTERNATE METHOD** 1 STATE V. MULTIPLE STATES**
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: '',
+  //   enteredAmount: '',
+  //   enteredDate: ''
+  // });
+
   const titleChangeHandler = (event) => {
-    console.log(event.target.value);
+    setEnteredTitle(event.target.value);
+
+    //ALTERNATE METHOD** WOULD NEED TO BE REPEATED FOR OTHER HANDLERS**
+    //**USE THIS WHEN STATE UPDATE DEPENDS ON PREVIOUS STATE */
+    // setUserInput({
+    //   ...userInput,
+    //   eneredTitle: event.target.value,
+    // });
+    // setUseInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value};
+    // });
   };
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log(expenseData);
+    //*TWO WAY BINDING (CLEARING FORM AFTER SUBMITTING)
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input tpye="text" onChange={titleChangeHandler} />
+          <input
+            tpye="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type='number' min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type='date' min="2022-01-01" max="2023-12-31" />
+          <input
+            type="date"
+            min="2022-01-01"
+            max="2023-12-31"
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
