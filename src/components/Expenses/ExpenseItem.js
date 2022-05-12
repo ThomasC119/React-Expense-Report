80c22939ca3030dd7b2c022b9941055d0cc57da1
ThomasC119 <90348324+ThomasC119@users.noreply.github.com>
2022-05-12 15:09:19 -0400

    implementing state
    moving from static to more dynamic pages

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, { useState } from "react";
+
 import ExpenseDate from "./ExpenseDate";
-import Card from '../UI/Card';
+import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  console.log('ExpenseItem evaulated by React');
+
   const clickHandler = () => {
-    console.log('clicked!');
+    setTitle('Updated!');
+    console.log(title);
   };
   return (
-    <Card className='expense-item'>
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
